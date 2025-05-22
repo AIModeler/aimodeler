35358f71ce430985f24831210d7937b71cf4d08f
AIModeler <ajbrownt@gmail.com>
2025-05-22 22:56:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='AIModeler',
-    version='0.9',
+    version='0.91',
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     install_requires=["PyMuPDF", "tiktoken"],
