a8f86f957fcc35ea7ba42dc7938c4e408c72e1a5
AI Modeler <ajbrownt@gmail.com>
2026-02-06 19:40:04 +0000

    Update foundation_models.py

--- src/aimodeler/foundation_models.py
+++ src/aimodeler/foundation_models.py
@@ -1,1 +1,14 @@
+import os
 
+def chatgpt_model:
+
+  from openai import OpenAI
+  import tiktoken
+
+  client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+  LLM_MODEL = "gpt-4.1-nano"
+
+  EMBED_MODEL = "text-embedding-3-small"
+
+  TOKENIZER = tiktoken.encoding_for_model(EMBED_MODEL)
