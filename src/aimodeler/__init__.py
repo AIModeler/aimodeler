a511f5a65ab5d23822b7cc13b0e6903f2b974820
AI Modeler <ajbrownt@gmail.com>
2026-02-06 20:23:39 +0000

    Update __init__.py

--- src/aimodeler/__init__.py
+++ src/aimodeler/__init__.py
@@ -1,5 +1,5 @@
 from .foundation_models import chatgpt_model
 
-from .data_connectors import cloud_drive
+from .data_connectors import object_storage
 
-__all__ = ["chatgpt_model", "cloud_drive"]
+__all__ = ["chatgpt_model", "object_storage"]
